2cce0c50e3036f154aceecd4711ffe58f0e68135
pasmi83 <pasmi83@gmail.com>
2020-06-30 12:57:12 +0300

    Добавлен profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=150, blank=True, default='')
+    about = models.TextField(default='', blank=True)
+    profile_image = models.ImageField(upload_to='profile_image/%Y/%m/%d',
+                                     default='default/default_profile_image.png',
+                                    verbose_name='Profile Image',
+                                   blank=True)
+    profile_image_thumbnail = models.ImageField(upload_to='profile_image_thumbnail/%Y/%m/%d', blank=True, verbose_name='Profile Image Thumbnail')
+    def __str__(self):
+        return self.user.get_full_name()
