f85b33232d6c454361b7a3dfaa083ee8e950e876
pasmi83 <pasmi83@gmail.com>
2020-07-06 15:18:18 +0300

    недобитый логин

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        context={}
+        return render(request,'accounts/login.html',context=context)
+    if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(request, username = username, password = password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('pages:index')
+            else:
+                return render(request,'accounts/login.html',context=context)
+def register(request):
+    context={}
+    return render(request,'accounts/register.html', context=context)
